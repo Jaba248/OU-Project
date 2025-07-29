a015ef80556ac5737bef803be088f1ab7782d400
Abdullah Imran <31128993+Jaba248@users.noreply.github.com>
2025-07-29 20:29:40 +0100

    enable credentials

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -30,6 +30,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", # Vite's default port
     "http://127.0.0.1:5173", # include direct local ip
 ]
+# Allow authentication
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
