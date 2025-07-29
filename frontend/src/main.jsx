db3ba6d246025d3846e4a3db7fbdaf5bebfeaf1a
Abdullah Imran <31128993+Jaba248@users.noreply.github.com>
2025-07-29 18:03:10 +0100

    Initiate Apollo client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+// Apollo Graphql imports
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-createRoot(document.getElementById('root')).render(
+// Create link to connect to our backend api
+const httpLink = createHttpLink({
+  uri: "http://localhost:8000/graphql",
+});
+
+// Add our jwt token when the link is initiated, so it is attached to every call
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem("authToken");
+  // Return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : "",
+    },
+  };
+});
+
+// Create the final Apollo Client instance
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </StrictMode>
+);
