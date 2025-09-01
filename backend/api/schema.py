cdc2f2c928da1393c53d601b2bdc79c8eba09981
Abdullah Imran <31128993+Jaba248@users.noreply.github.com>
2025-09-01 14:41:01 +0100

    Update User creation to include name fields and use email as the username

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -90,13 +90,21 @@ class CreateUser(graphene.Mutation):
     user = graphene.Field(UserType)
 
     class Arguments:
-        username = graphene.String(required=True)
+        first_name = graphene.String(required=True)
+        last_name = graphene.String(required=True)
         password = graphene.String(required=True)
         email = graphene.String(required=True)
 
-    def mutate(self, info, username, password, email):
+    def mutate(self, info, password, email, first_name, last_name):
         #  todo raise more specific errors for, email already exists, username already exists, password not secure enough
-        user = get_user_model()(username=username, email=email)
+        user, created = get_user_model().objects.get_or_create(
+            email=email,
+            username=email,
+            defaults={"first_name": first_name, "last_name": last_name},
+        )
+        if not created:
+            raise Exception("An account already exists with that email!")
+
         user.set_password(password)
         user.save()
         return CreateUser(user=user)
