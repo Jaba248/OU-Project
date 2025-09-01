f844291dd137df3c3c322c34d21328dd4204dc7a
Abdullah Imran <31128993+Jaba248@users.noreply.github.com>
2025-09-01 14:32:45 +0100

    Change buttons in navbar if user is logged in

--- frontend/src/components/LandingLayout.jsx
+++ frontend/src/components/LandingLayout.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 const LandingLayout = () => {
+  const { isLoggedIn } = useAuth();
   return (
     <div className="font-sans">
       <header className="bg-white shadow-md">
@@ -10,18 +12,30 @@ const LandingLayout = () => {
             <Link to="/">Freelancer PMS</Link>
           </div>
           <div className="space-x-4">
-            <Link
-              to="/login"
-              className="px-4 py-2 text-gray-600 hover:text-blue-500"
-            >
-              Login
-            </Link>
-            <Link
-              to="/register"
-              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-            >
-              Register
-            </Link>
+            {isLoggedIn ? (
+              <Link
+                to="/dashboard/"
+                className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+              >
+                Dashboard
+              </Link>
+            ) : (
+              <>
+                {" "}
+                <Link
+                  to="/login"
+                  className="px-4 py-2 text-gray-600 hover:text-blue-500"
+                >
+                  Login
+                </Link>
+                <Link
+                  to="/register"
+                  className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                >
+                  Register
+                </Link>
+              </>
+            )}
           </div>
         </nav>
       </header>
