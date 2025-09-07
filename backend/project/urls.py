6db112a1af29a3edd5604e47d6e2db86b1843b1d
Abdullah Imran <31128993+Jaba248@users.noreply.github.com>
2025-09-07 21:47:33 +0100

    Set GraphiQL to only show when debug is enabled

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -19,9 +19,10 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=settings.DEBUG))),
     # CSRF exempt used to prevent access denied without csrf token
 ]
